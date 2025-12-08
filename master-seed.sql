da2e12ad639e520c30075625f5aeafab0428128b
xhuyz <xhuyzdev@gmail.com>
2025-12-09 03:34:51 +0700

    update

--- master-seed.sql
+++ master-seed.sql
@@ -1,14 +1,10 @@
-\connect AuthDB;
-\i './AuthDB_seed.sql';
-
-\connect UserDB;
-\i './UserDB_seed.sql';
-
-\connect TripDB;
-\i './TripDB_seed.sql';
-
-\connect PaymentDB;
-\i './PaymentDB_seed.sql';
-
-\connect ChatDB;
-\i './ChatDB_seed.sql';
+\CONNECTAuthDB;
+\i'./authnew.sql.';
+\CONNECTUserDB;
+\i'./usernew.sql';
+\CONNECTTripDB;
+\i'./tripfinally.sql';
+\CONNECTPaymentDB;
+\i'./paymentfinally.sql';
+\CONNECTChatDB;
+\i'./chatfinal.sql';
