eca75152531377c32b8d643fe6c25d969f4d9009
XhuyZ <xhuyzdev@gmail.com>
2025-12-08 12:33:53 -0800

    Add connections and seed files for multiple databases
    Added database connection and seed file execution for multiple databases.

--- master-seed.sql
+++ master-seed.sql
@@ -1,2 +1,14 @@
-SELECT
+\connect AuthDB;
+\i './AuthDB_seed.sql';
 
+\connect UserDB;
+\i './UserDB_seed.sql';
+
+\connect TripDB;
+\i './TripDB_seed.sql';
+
+\connect PaymentDB;
+\i './PaymentDB_seed.sql';
+
+\connect ChatDB;
+\i './ChatDB_seed.sql';
