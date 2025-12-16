71a07967ed2dfa7aff64c748c648a3648bcc0bb0
XhuyZ <xhuyzdev@gmail.com>
2025-12-16 10:02:51 +0700

    Insert new user roles and users into database
    Added new user roles and users to the database.

--- authnew.sql
+++ authnew.sql
@@ -1,11 +1,4 @@
---
--- PostgreSQL database dump
---
 
--- Dumped from database version 16.10 (Ubuntu 16.10-0ubuntu0.24.04.1)
--- Dumped by pg_dump version 17.0
-
--- Started on 2025-12-06 07:27:50
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -17,30 +10,12 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- TOC entry 3451 (class 0 OID 17144)
--- Dependencies: 216
--- Data for Name: AspNetRoles; Type: TABLE DATA; Schema: public; Owner: -
---
 
 INSERT INTO public."AspNetRoles" VALUES ('4f719b01-2d11-455e-804c-3ee7eb46f6ee', 'Khách du lịch ', '2025-10-31 08:46:32.744908+07', '2025-10-31 08:46:32.744476+07', NULL, 'Traveller', 'TRAVELLER', '82be28d5-8b1c-489d-a448-e53d56af91a3');
 INSERT INTO public."AspNetRoles" VALUES ('6fd44f5e-2e85-482c-8acc-c415e3272edc', 'Đại diện du lịch', '2025-10-31 08:48:06.145355+07', '2025-10-31 08:48:06.14528+07', NULL, 'TravelRepresentative', 'TRAVELREPRESENTATIVE', 'b6a364cd-c713-4306-93d5-6b6b9a9b78ec');
 INSERT INTO public."AspNetRoles" VALUES ('879b4062-63e2-42db-87db-3ae053aca7d4', 'Admin', '2025-10-31 08:48:30.075464+07', '2025-10-31 08:48:30.075432+07', NULL, 'Admin', 'ADMIN', 'f4a14413-3f60-4788-8a24-66b69043a22c');
+INSERT INTO public."AspNetRoles" VALUES ('774a88b4-01f2-460e-b762-733a33f666e7', 'Nhân viên', '2025-12-10 04:29:16.238105 +00:00', '2025-12-10 04:29:16.237790 +00:00', NULL, 'Staff', 'STAFF', 'aecb9225-46e8-4a82-b6b4-495d008f0625');
 
-
---
--- TOC entry 3454 (class 0 OID 17159)
--- Dependencies: 219
--- Data for Name: AspNetRoleClaims; Type: TABLE DATA; Schema: public; Owner: -
---
-
-
-
---
--- TOC entry 3452 (class 0 OID 17151)
--- Dependencies: 217
--- Data for Name: AspNetUsers; Type: TABLE DATA; Schema: public; Owner: -
---
 
 INSERT INTO public."AspNetUsers" VALUES ('f0becb5b-0871-4d46-900f-4457566f7306', 'admingt@gmail.com', 'active', '12345678', 'Agribank', false, '2025-10-31 08:50:59.652+07', '2025-10-31 08:50:59.652+07', NULL, '2025-10-31 08:50:59.652+07', NULL, 'admingt@gmail.com', 'ADMIN1@EXAMPLE.COM', 'admin1@example.com', 'ADMIN1@EXAMPLE.COM', true, 'AQAAAAIAAYagAAAAEKgK7zPvVTPpoZWq0q1HvyrmqNq2Q44Ojl6MK+ehjO3H3wPbYNTf2+b+pjggYfu4bw==', NULL, NULL, '0944688134', true, false, NULL, true, 0);
 INSERT INTO public."AspNetUsers" VALUES ('73098bfc-daf7-415b-b69e-57b460c0bd9b', 'Trương Minh Hiếu', 'active', '12345678', 'BIDV', false, '2025-12-05 23:23:48.599787+07', '2025-12-06 06:30:38.748262+07', NULL, '2025-12-05 23:23:48.599768+07', NULL, 'zhuyxdeb@gmail.com', 'ZHUYXDEB@GMAIL.COM', 'zhuyxdeb@gmail.com', 'ZHUYXDEB@GMAIL.COM', true, 'AQAAAAIAAYagAAAAELhS8RzfxSBDK7FIpWHtFMahH4YOSBuI6FL37TJDPMcDmOTQ4W0MjlGVL9ligtqKVw==', 'ELSGABST75P56Q3BAIOJQRQDSPH2G6KK', '75475490-4803-40a9-8e2c-1a5ee5af48f8', NULL, true, false, NULL, true, 0);
@@ -54,28 +29,10 @@ INSERT INTO public."AspNetUsers" VALUES ('e0d4f7c1-c514-401d-b2a0-9ed51b25917a',
 INSERT INTO public."AspNetUsers" VALUES ('367ad2ed-eb08-44f4-9317-8148a534686d', 'Nguyễn Thị Ngọc Thư', 'active', 'string', 'string', false, '2025-12-06 20:52:27.821086+07', '2025-12-06 20:52:27.821086+07', NULL, '2025-12-06 20:52:27.821065+07', NULL, 'nthu18313@gmail.com', 'NTHU18313@GMAIL.COM', 'nthu18313@gmail.com', 'NTHU18313@GMAIL.COM', true, 'AQAAAAIAAYagAAAAEMlMqJWPPsi++16zM1EL/3P4kqeo6U88NHBrWZt6QxeEy+AG0g4MMD5/OLqbIBbBMw==', 'ZRZCV4YGZUTBIYPMW7YF5YEJKA5ELWJ2', '6e86bc84-12a1-43de-ad47-20c35fb3901b', NULL, true, false, NULL, true, 0);
 INSERT INTO public."AspNetUsers" VALUES ('de170f49-71ef-45a2-9af4-14b3fc5295ef', 'Lý Thiên Long', 'active', '0993812455521', 'BIDV', false, '2025-12-06 18:21:03.471151+07', '2025-12-07 01:23:55.89085+07', NULL, '2025-12-06 18:21:03.471143+07', NULL, 'appselfra@gmail.com', 'APPSELFRA@GMAIL.COM', 'appselfra@gmail.com', 'APPSELFRA@GMAIL.COM', true, 'AQAAAAIAAYagAAAAEFLQmhrscEXbHaZqzk+6EeB1ftRY6Dv95ylcK2dx6V5cd8QgyjYLi1BtYYLAXfbpTA==', 'MVASLDXUPZDRHZMKUNAVXYUEKIQ7BGG3', '03e0dca6-4cd0-4a9d-a007-edadbe62e8e2', NULL, true, false, NULL, true, 0);
 
+INSERT INTO public."AspNetUsers" VALUES ('943303b8-0b5d-49ad-89a3-a004cbcff48c', 'Minh Tị', 'active', '0993812455521', 'BIDV', false, '2025-12-12 06:35:39.159366 +00:00', '2025-12-12 06:35:39.159366 +00:00', NULL, '2025-12-06 18:21:03.471143+07', NULL, 'hoangvv111', 'HOANGVV111', 'hoangvv111@gmail.com', 'HOANGVV111@GMAIL.COM', true, 'AQAAAAIAAYagAAAAEHD+NwUKdJkIyZWnOOrG05ju23LrpZH5o5e7iW9lI9/pOQqYlUUX+6NMWWZ/Ajr2GA==', 'FXIRGZUPHNVPLQCXPNCE23IT4CCD7KBI', '49a6b6cd-c884-4d7d-9f43-ce63587cbaa8', NULL, true, false, NULL, true, 0);
+INSERT INTO public."AspNetUsers" VALUES ('db6898b6-f3f4-48a3-b0ed-8e089035cc10', 'Minh Tí', 'active', '0993812455521', 'BIDV', false, '2025-12-06 18:21:03.471151+07', '2025-12-07 01:23:55.89085+07', NULL, '2025-12-06 18:21:03.471143+07', NULL, 'damoniee4267', 'DAMONIEE4267', 'damoniee4267@gmail.com', 'DAMONIEE4267@GMAIL.COM', true, 'AQAAAAIAAYagAAAAEKOxbJ+4GmTd9lqIZiitnL82YjJEiSukxisyNVpXPmSIdrDJJXvok2fADqn2SLYuZw==', '7ZJBSFUCI2CBEM6ALCYNAOC4K3HU233B', 'ae18eda8-697f-4fd6-a28e-b212b4ad6863', NULL, true, false, NULL, true, 0);
+INSERT INTO public."AspNetUsers" VALUES ('e0cf73eb-518a-4962-902b-31a360a8b831', 'Minh Tèo', 'active', '0993812455521', 'BIDV', false, '2025-12-06 18:21:03.471151+07', '2025-12-07 01:23:55.89085+07', NULL, '2025-12-06 18:21:03.471143+07', NULL, 'andang', 'ANDANG', 'andang@gmail.com', 'ANDANG@GMAIL.COM', true, 'AQAAAAIAAYagAAAAEOiGmzCC4oIy2YMUkMUhi7C+IwxsE7Zliderjz9YTSQ7oDvWhpRmus9EmPdPIagecA==', 'VJDSFLC5SGLFZ45IBCPSABCUUJXI2LID', 'b3908383-ba0a-4a45-82ee-0ba809237313', NULL, true, false, NULL, true, 0);
 
---
--- TOC entry 3456 (class 0 OID 17172)
--- Dependencies: 221
--- Data for Name: AspNetUserClaims; Type: TABLE DATA; Schema: public; Owner: -
---
-
-
-
---
--- TOC entry 3457 (class 0 OID 17184)
--- Dependencies: 222
--- Data for Name: AspNetUserLogins; Type: TABLE DATA; Schema: public; Owner: -
---
-
-
-
---
--- TOC entry 3458 (class 0 OID 17196)
--- Dependencies: 223
--- Data for Name: AspNetUserRoles; Type: TABLE DATA; Schema: public; Owner: -
---
 
 INSERT INTO public."AspNetUserRoles" VALUES ('f0becb5b-0871-4d46-900f-4457566f7306', '879b4062-63e2-42db-87db-3ae053aca7d4', '2025-12-03 18:24:47.865+07', '2025-12-03 18:24:47.865+07', NULL);
 INSERT INTO public."AspNetUserRoles" VALUES ('df790517-7517-4172-aeb1-af0de8d1acf3', '6fd44f5e-2e85-482c-8acc-c415e3272edc', '2025-12-05 23:10:36.000137+07', '2025-12-05 23:10:36.000137+07', NULL);
@@ -89,27 +46,12 @@ INSERT INTO public."AspNetUserRoles" VALUES ('e0d4f7c1-c514-401d-b2a0-9ed51b2591
 INSERT INTO public."AspNetUserRoles" VALUES ('de170f49-71ef-45a2-9af4-14b3fc5295ef', '6fd44f5e-2e85-482c-8acc-c415e3272edc', '2025-12-06 18:21:03.693116+07', '2025-12-06 18:21:03.693116+07', NULL);
 INSERT INTO public."AspNetUserRoles" VALUES ('367ad2ed-eb08-44f4-9317-8148a534686d', '4f719b01-2d11-455e-804c-3ee7eb46f6ee', '2025-12-06 20:52:28.459742+07', '2025-12-06 20:52:28.459742+07', NULL);
 
-
---
--- TOC entry 3459 (class 0 OID 17211)
--- Dependencies: 224
--- Data for Name: AspNetUserTokens; Type: TABLE DATA; Schema: public; Owner: -
---
+INSERT INTO public."AspNetUserRoles" VALUES ('943303b8-0b5d-49ad-89a3-a004cbcff48c', '774a88b4-01f2-460e-b762-733a33f666e7', '2025-12-06 20:52:28.459742+07', '2025-12-06 20:52:28.459742+07', NULL);
+INSERT INTO public."AspNetUserRoles" VALUES ('db6898b6-f3f4-48a3-b0ed-8e089035cc10', '774a88b4-01f2-460e-b762-733a33f666e7', '2025-12-06 20:52:28.459742+07', '2025-12-06 20:52:28.459742+07', NULL);
+INSERT INTO public."AspNetUserRoles" VALUES ('e0cf73eb-518a-4962-902b-31a360a8b831', '774a88b4-01f2-460e-b762-733a33f666e7', '2025-12-06 20:52:28.459742+07', '2025-12-06 20:52:28.459742+07', NULL);
 
 
 
---
--- TOC entry 3450 (class 0 OID 17139)
--- Dependencies: 215
--- Data for Name: __EFMigrationsHistory; Type: TABLE DATA; Schema: public; Owner: -
---
-
-
-
---
--- TOC entry 3465 (class 0 OID 0)
--- Dependencies: 218
--- Name: AspNetRoleClaims_Id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
 SELECT pg_catalog.setval('public."AspNetRoleClaims_Id_seq"', 1, false);
